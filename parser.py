342362b69d28e53661957c2acd3d2859ea17e95d
curtis goud <cgoud@ualberta.ca>
2018-11-25 02:43:37 -0700

    Apparenty a more efficient way to open the file in memory

--- parser.py
+++ parser.py
@@ -34,29 +34,29 @@ def generateTerms(xmlfile):
     # create the terms.txt file
     termsFile = open("terms.txt", "w+")
     # open the xml file and read from it line by line
-    xml = open(xmlfile, "r")
     # process each of the lines
-    for line in enumerate(xml):
-        if getInformation("ad", line[1]) is not "":
-            # the line needs to be parsed
-            titleString = getInformation("ti", line[1])
-            description = getInformation("desc", line[1])
-            totalString = titleString + " " + description
-            id = getInformation("aid", line[1])
-            # get a list of the terms along with the identy of the ad
-            # set all of the compile patterns
-            replaceStr = re.compile("&#[0-9]+|&.*;")
-            splitStr = re.compile(" +|, +")
-            checkTerm = re.compile("^[0-9a-zA-Z_-]+$")
+    with open(xmlfile,"r") as xml:
+        for line in enumerate(xml):
+            if getInformation("ad", line[1]) is not "":
+                # the line needs to be parsed
+                titleString = getInformation("ti", line[1])
+                description = getInformation("desc", line[1])
+                totalString = titleString + " " + description
+                id = getInformation("aid", line[1])
+                # get a list of the terms along with the identy of the ad
+                # set all of the compile patterns
+                replaceStr = re.compile("&#[0-9]+|&.*;")
+                splitStr = re.compile(" +|, +")
+                checkTerm = re.compile("^[0-9a-zA-Z_-]+$")
 
-            totalString = replaceStr.sub("", totalString)
+                totalString = replaceStr.sub("", totalString)
 
-            totalTerms = list(set(splitStr.split(totalString)))
-            for term in totalTerms:
-                if len(term) > 2 and checkTerm.match(term) is not None:
-                    # good to write to the terms.txt file
-                    termsFile.write(str.lower(term) + ":" + id + "\r\n")
-                    #print(str.lower(term) + ":" + id) # make sure it works right
+                totalTerms = list(set(splitStr.split(totalString)))
+                for term in totalTerms:
+                    if len(term) > 2 and checkTerm.match(term) is not None:
+                        # good to write to the terms.txt file
+                        termsFile.write(str.lower(term) + ":" + id + "\r\n")
+                        #print(str.lower(term) + ":" + id) # make sure it works right
     xml.close()
     termsFile.close()
     return
@@ -68,48 +68,48 @@ def generatePDates(xmlfile):
     # create the pdates.txt file
     pdatesFile = open("pdates.txt", "w+")
     # open the xml file
-    xml = open(xmlfile, "r")
 
     # process each of the lines
-    for line in enumerate(xml):
-        if getInformation("ad", line[1]) is not "":
-            # get the desired information
-            date = getInformation("date", line[1])
-            adID = getInformation("aid", line[1])
-            category = getInformation("cat",line[1])
-            location = getInformation("loc",line[1])
-            pdatesFile.write(date + ":" + adID + "," + category + "," + location + "\r\n")
-            #print(date + ":" + adID + "," + category + "," + location + "\r\n")
+    with open(xmlfile,"r") as xml:
+        for line in enumerate(xml):
+            if getInformation("ad", line[1]) is not "":
+                # get the desired information
+                date = getInformation("date", line[1])
+                adID = getInformation("aid", line[1])
+                category = getInformation("cat",line[1])
+                location = getInformation("loc",line[1])
+                pdatesFile.write(date + ":" + adID + "," + category + "," + location + "\r\n")
+                #print(date + ":" + adID + "," + category + "," + location + "\r\n")
     pdatesFile.close()
     xml.close()
     return
 
 def generatePrices(file):
     #TODO: Change file name later
-    fpr = open(file)
     fpw = open("prices.txt","w+")
-    for line in enumerate(fpr):
-        if getInformation("ad",line[1]) is not "":
-            price = getInformation("price",line[1])
-            adID = getInformation("aid",line[1])
-            category = getInformation("cat",line[1])
-            location = getInformation("loc",line[1])
-            fpw.write(price + ":" + adID + "," + category + "," + location + "\r\n")
-            #print(price + ":" + adID + "," + category + "," + location)
+    with open(file,"r") as fpr:
+        for line in enumerate(fpr):
+            if getInformation("ad",line[1]) is not "":
+                price = getInformation("price",line[1])
+                adID = getInformation("aid",line[1])
+                category = getInformation("cat",line[1])
+                location = getInformation("loc",line[1])
+                fpw.write(price + ":" + adID + "," + category + "," + location + "\r\n")
+                #print(price + ":" + adID + "," + category + "," + location)
     fpr.close()
     fpw.close()
     return
 
 def generateAds(file):
     #TODO: Change file name later
-    fpr = open(file)
     fpw = open("ads.txt","w+")
-    for line in enumerate(fpr):
-        if getInformation("ad",line[1]) is not "":
-            adID = getInformation("aid",line[1])
-            ad = "<ad>" + getInformation("ad",line[1]) + "</ad>"
-            fpw.write(adID + ":" + ad + "\r\n")
-            #print(adID + ":" + ad)
+    with open(file,"r") as fpr:
+        for line in enumerate(fpr):
+            if getInformation("ad",line[1]) is not "":
+                adID = getInformation("aid",line[1])
+                ad = "<ad>" + getInformation("ad",line[1]) + "</ad>"
+                fpw.write(adID + ":" + ad + "\r\n")
+                #print(adID + ":" + ad)
     fpr.close()
     fpw.close()
     return
