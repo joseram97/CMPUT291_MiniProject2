964fc718ca775ae1c8fb3cc65334bafb6c032825
joseram97 <jose@ualberta.ca>
2018-11-25 17:02:12 -0700

    Started on the main project with the basic interface

--- main_project.py
+++ main_project.py
@@ -4,24 +4,65 @@
 # Definition: The program will ask for a file to parse. Then it will run the
 # desired queries and output the data, as well as some stats
 
+# may not need these
 import sqlite3
 import time
-# lxml is the main parser that we are using for better simplicity
-from lxml import *
-from parser import *
+# for the regex if required
+import re
+
+def printQuery(queryResult):
+    # Parameters:
+    #   queryResult - is the list of rows from the query result
+    print(queryResult)
+    return
+
+def query(output, condition):
+    # Parameters:
+    #   output - the output flag of how much to query. Either "full" or "bried"
+    #   condition - The condition statement of what to query for
+    return "Under Construction"
+
+def main_loop():
+    # start the while loop for the application. The user will be prompted to input
+    # a specific format in order to ensure the query is correct
+    print("**Must input in the format 'output=full/brief condition'**")
+    print("**To leave the application please type 'exit'\n")
+    # set any variables
+    while (True):
+        # prompt the user for input
+        inputStr = input(">> ")
+        if inputStr == "exit":
+            #leave the application
+            break
+        # get the split arguments. We are going to assume that the user put in the
+        # correct format
+        outputArg = inputStr[:inputStr.find(" ")]
+        conditionArg = inputStr[inputStr.find(" ")+1:]
+        print("Output argument: " + outputArg)
+        print("Condition argument: " + conditionArg)
+
+
+        # get the description of the flag
+        outputFlag = outputArg.split("=")[1]
+        queryResult = query(outputFlag, conditionArg)
+        printQuery(queryResult)
+
+
+    return
 
 def main():
-    #the main code for the software applications
-    #enable a print to ask the user for their username
-    initializeData()
+    # handle any initialization and make sure that the the files have been created
+    print("Welcome to the query interface! Please ensure that the following files")
+    print("Have been created:")
+    print("- ad.idx")
+    print("- da.idx")
+    print("- pr.idx")
+    print("- te.idx\n")
+    print("If not please make those files with the parser.py and the index.py program.\n")
 
-    # show the login prompt
-    if not (loginPrompt()):
-        return
+    main_loop()
 
-    # the login was succesful
-    # run the main portion of the code
-    runApp()
+    print("Leaving the query interface application...Good bye!")
     return
 
 if __name__ == "__main__":
